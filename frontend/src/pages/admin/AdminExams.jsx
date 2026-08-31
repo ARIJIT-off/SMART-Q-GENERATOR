eea056ba63fb8bfb18172d2897ae3e3a534f9b8e
ARIJIT-off <arijitp203@gmail.com>
2026-08-31 22:22:18 +0530

    fix: removed duplicate Link import in AdminExams.jsx

--- frontend/src/pages/admin/AdminExams.jsx
+++ frontend/src/pages/admin/AdminExams.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { QRCodeSVG } from 'qrcode.react';
 import api from '../../utils/api';
 import toast from 'react-hot-toast';
