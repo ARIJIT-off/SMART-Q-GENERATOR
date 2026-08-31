0b47e627cdac7a6191f41b69933bdaeaa0dcbd35
ARIJIT-off <arijitp203@gmail.com>
2026-08-31 21:22:13 +0530

    fix: removed manualChunks to resolve Rolldown build error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,13 +19,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          charts: ['recharts'],
-        }
-      }
-    }
+
   }
 })
